d33b2ff721181aaacb2682ff9bc8da0a885d0e86
SneakySN <03230323yh@gmail.com>
2024-05-10 01:04:20 +0900

    Update SpecifiedDietActivity.java, placeholders for each buttons

--- app/src/main/java/com/example/petwithdietmanagement/SpecifiedDietActivity.java
+++ app/src/main/java/com/example/petwithdietmanagement/SpecifiedDietActivity.java
@@ -1,8 +1,10 @@
 package com.example.petwithdietmanagement;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,15 +16,41 @@ import com.example.petwithdietmanagement.PetMenuActivity;
 import com.example.petwithdietmanagement.MainActivity;
 import com.example.petwithdietmanagement.R;
 
+import java.util.Objects;
+
 public class SpecifiedDietActivity extends AppCompatActivity {
+    private int curStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // 각 버튼 함수 내에서 구현할 것들 : 버튼을 누를 시 .json 파일에서 데이터 불러오기
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_specifieddiet); // XML 레이아웃 이름을 입력하세요
+        setContentView(R.layout.activity_specifieddiet);
+        ImageButton homeButton = findViewById(R.id.ic_home);
+        ImageButton dietButton = findViewById(R.id.ic_diet);
+        ImageButton calendarButton = findViewById(R.id.ic_calendar);
+        ImageButton petMenuButton = findViewById(R.id.ic_petMenu);
+        ImageButton menuButton = findViewById(R.id.menuButton);
+        Button recButton = findViewById(R.id.button_recommended);
+        Button korButton = findViewById(R.id.button_korean);
+        Button wesButton = findViewById(R.id.button_western);
+        Button japButton = findViewById(R.id.button_japanese);
+
+        Intent choiceIntent = getIntent();
+        String choice = choiceIntent.getStringExtra("tag");
+
+        if (Objects.equals(choice, "0")) {
+            korButton.setBackgroundColor(Color.parseColor("#80000000"));
+            curStatus = 1;
+        } else if (Objects.equals(choice, "1")) {
+            wesButton.setBackgroundColor(Color.parseColor("#80000000"));
+            curStatus = 2;
+        } else if (Objects.equals(choice, "2")) {
+            japButton.setBackgroundColor(Color.parseColor("#80000000"));
+            curStatus = 3;
+        }
 
         // 홈 버튼
-        ImageButton homeButton = findViewById(R.id.ic_home);
         homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,7 +60,6 @@ public class SpecifiedDietActivity extends AppCompatActivity {
         });
 
         // 음식 버튼
-        ImageButton dietButton = findViewById(R.id.ic_diet);
         dietButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,7 +69,6 @@ public class SpecifiedDietActivity extends AppCompatActivity {
         });
 
         // 캘린더 버튼
-        ImageButton calendarButton = findViewById(R.id.ic_calendar);
         calendarButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -52,7 +78,6 @@ public class SpecifiedDietActivity extends AppCompatActivity {
         });
 
         // 펫 메뉴 버튼
-        ImageButton petMenuButton = findViewById(R.id.ic_petMenu);
         petMenuButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -62,12 +87,39 @@ public class SpecifiedDietActivity extends AppCompatActivity {
         });
 
         // 메뉴 버튼
-        ImageButton menuButton = findViewById(R.id.menuButton);
         menuButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SpecifiedDietActivity.this, MenuPageActivity.class); // 메뉴 페이지로 이동
                 startActivity(intent);
+            }
+        });
+
+        recButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curStatus = 0;
+            }
+        });
+
+        korButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curStatus = 1;
+            }
+        });
+
+        wesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curStatus = 2;
+            }
+        });
+
+        japButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curStatus = 3;
             }
         });
     }
